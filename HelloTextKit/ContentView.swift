414b133ea0d7d45e6270f84e7dfaed4a723a2745
drypot <drypot@gmail.com>
2026-02-16 15:03:03 +0900

    Initial Commit

---
+++ HelloTextKit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HelloTextKit
+//
+//  Created by Kyuhyun Park on 2/16/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
